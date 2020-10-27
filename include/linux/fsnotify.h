dcb0b699142f1a07b5aebc68c91ba1f31db82fea
Alessio Balsini <balsini@google.com>
2020-10-27 13:24:03 +0000

    ANDROID: vfs: add d_canonical_path for stacked filesystem support
    Inotify does not currently know when a filesystem is acting as a wrapper
    around another fs. This means that inotify watchers will miss any
    modifications to the base file, as well as any made in a separate stacked
    fs that points to the same file.
    d_canonical_path solves this problem by allowing the fs to map a dentry to
    a path in the lower fs. Inotify can use it to find the appropriate place to
    watch to be informed of all changes to a file.
    
    Test: Pixel 4.19
    Bug: 171780975
    Change-Id: I09563baffad1711a045e45c1bd0bd8713c2cc0b6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    [astrachan: Folded 34df4102216e ("ANDROID: fsnotify: Notify lower fs of
                open") into this patch]
    Signed-off-by: Alistair Strachan <astrachan@google.com>
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Signed-off-by: Alessio Balsini <balsini@google.com>

--- include/linux/fsnotify.h
+++ include/linux/fsnotify.h
@@ -223,6 +223,14 @@ static inline void fsnotify_open(struct file *file)
 	if (S_ISDIR(inode->i_mode))
 		mask |= FS_ISDIR;
 
+	if (path->dentry->d_op && path->dentry->d_op->d_canonical_path) {
+		path->dentry->d_op->d_canonical_path(path, &lower_path);
+		fsnotify_parent(&lower_path, NULL, mask);
+		fsnotify(lower_path.dentry->d_inode, mask, &lower_path,
+			 FSNOTIFY_EVENT_PATH, NULL, 0);
+		path_put(&lower_path);
+	}
+
 	fsnotify_parent(path, NULL, mask);
 	fsnotify(inode, mask, path, FSNOTIFY_EVENT_PATH, NULL, 0);
 }
