ab9465618ce7f2f65f4200a5fb73152b8b0782a9
Xuewen Yan <xuewen.yan@unisoc.com>
2025-02-19 17:37:46 +0800

    sched/uclamp: Use the uclamp_is_used() helper instead of open-coding it
    Don't open-code static_branch_unlikely(&sched_uclamp_used), we have
    the uclamp_is_used() wrapper around it.
    
    [ mingo: Clean up the changelog ]
    
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Hongyan Xia <hongyan.xia2@arm.com>
    Reviewed-by: Christian Loehle <christian.loehle@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20250219093747.2612-1-xuewen.yan@unisoc.com
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: rssu. <rissu.ntk@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1193,7 +1193,7 @@ static inline void uclamp_rq_inc(struct rq *rq, struct task_struct *p, int flags
 	 * The condition is constructed such that a NOP is generated when
 	 * sched_uclamp_used is disabled.
 	 */
-	if (!static_branch_unlikely(&sched_uclamp_used))
+	if (!uclamp_is_used())
 		return;
 
 	if (unlikely(!p->sched_class->uclamp_enabled))
@@ -1221,7 +1221,7 @@ static inline void uclamp_rq_dec(struct rq *rq, struct task_struct *p)
 	 * The condition is constructed such that a NOP is generated when
 	 * sched_uclamp_used is disabled.
 	 */
-	if (!static_branch_unlikely(&sched_uclamp_used))
+	if (!uclamp_is_used())
 		return;
 
 	if (unlikely(!p->sched_class->uclamp_enabled))
