a48e42a5049533c5a331e8ffc2b5f65f2cef114a
Sandeep Dhavale <dhavale@google.com>
2026-05-27 19:05:20 -0700

    UPSTREAM: ANDROID: fuse-bpf: instantiate child dentry with correct child inode
    Ensure that FUSE-BPF backing handlers for mknod, link, and symlink
    correctly resolve and associate the child's backing inode rather
    than the parent's during dentry instantiation. This guarantees
    correct dcache mapping identity and prevents unintended
    re-initialization of cached parent inodes.
    
    Implement this by:
    1. Passing the correct child backing inode and 0 nodeid to
       `fuse_iget_backing` in `fuse_mknod_backing`, `fuse_link_backing`,
       and `fuse_symlink_backing`.
    2. Modifying `fuse_iget_backing` to only call `fuse_init_inode` if the
       inode is newly allocated (`I_NEW`), ensuring spinlocks are only
       initialized on fresh inodes.
    
    Bug: 508703026
    
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>
    Change-Id: I18bdf077f52fcbe241ab311a1fcd53940abfd3bf
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- fs/fuse/backing.c
+++ fs/fuse/backing.c
@@ -1461,7 +1461,7 @@ int fuse_mknod_backing(
 		 */
 		goto out;
 	}
-	inode = fuse_iget_backing(dir->i_sb, fuse_inode->nodeid, backing_inode);
+	inode = fuse_iget_backing(dir->i_sb, 0, d_inode(backing_path.dentry));
 	if (IS_ERR(inode)) {
 		err = PTR_ERR(inode);
 		goto out;
@@ -1892,7 +1892,7 @@ int fuse_link_backing(struct fuse_bpf_args *fa, struct dentry *entry,
 		goto out;
 	}
 
-	fuse_new_inode = fuse_iget_backing(dir->i_sb, fuse_dir_inode->nodeid, backing_dir_inode);
+	fuse_new_inode = fuse_iget_backing(dir->i_sb, 0, d_inode(backing_new_path.dentry));
 	if (IS_ERR(fuse_new_inode)) {
 		err = PTR_ERR(fuse_new_inode);
 		goto out;
@@ -2280,7 +2280,7 @@ int fuse_symlink_backing(
 		 */
 		goto out;
 	}
-	inode = fuse_iget_backing(dir->i_sb, fuse_inode->nodeid, backing_inode);
+	inode = fuse_iget_backing(dir->i_sb, 0, d_inode(backing_path.dentry));
 	if (IS_ERR(inode)) {
 		err = PTR_ERR(inode);
 		goto out;
