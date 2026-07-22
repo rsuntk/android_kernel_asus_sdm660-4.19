0d2aaa8ca91ccd7e6969194715d26072ad07157d
rsuntk <rissu.ntk@gmail.com>
2026-07-22 21:58:36 +0700

    sensors: stk3x1x: Disable log spam
    Change-Id: I1296cbeec60924668977b6bb7df074affb84bbd4
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- drivers/sensors/stk3x1x/stk3x1x.c
+++ drivers/sensors/stk3x1x/stk3x1x.c
@@ -45,6 +45,20 @@
 //#include <linux/earlysuspend.h>
 #endif
 
+#ifndef DEBUG
+
+#ifdef pr_info
+#undef pr_info
+#define pr_info(fmt, ...) no_printk(KERN_INFO fmt, ##__VA_ARGS__)
+#endif
+
+#ifdef pr_debug
+#undef pr_debug
+#define pr_debug(fmt, ...) no_printk(KERN_DEBUG fmt, ##__VA_ARGS__)
+#endif
+
+#endif
+
 #define DRIVER_VERSION "3.11.0"
 
 /* Driver Settings */
