ea10a3a22f43de767104cf7f3c9002902bf80d7a
rsuntk <rissu.ntk@gmail.com>
2026-08-02 16:47:55 +0700

    Revert "net: netfilter: Disable nf_log_trace"
    This reverts commit f2e137cadd747ae64621f13d21e62ca5f1e80077.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- net/ipv4/netfilter/ip_tables.c
+++ net/ipv4/netfilter/ip_tables.c
@@ -146,7 +146,6 @@ static const char *const comments[] = {
 	[NF_IP_TRACE_COMMENT_POLICY]	= "policy",
 };
 
-#ifdef CONFIG_DEBUG_KERNEL
 static const struct nf_loginfo trace_loginfo = {
 	.type = NF_LOG_TYPE_LOG,
 	.u = {
@@ -156,7 +155,6 @@ static const struct nf_loginfo trace_loginfo = {
 		},
 	},
 };
-#endif
 
 /* Mildly perf critical (only if packet tracing is on) */
 static inline int
@@ -213,11 +211,9 @@ static void trace_packet(struct net *net,
 		    &chainname, &comment, &rulenum) != 0)
 			break;
 
-#ifdef CONFIG_DEBUG_KERNEL
 	nf_log_trace(net, AF_INET, hook, skb, in, out, &trace_loginfo,
 		     "TRACE: %s:%s:%s:%u ",
 		     tablename, chainname, comment, rulenum);
-#endif
 }
 #endif
 
