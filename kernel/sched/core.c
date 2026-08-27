fbbfb5d78963761f1f9fdf7b191d69c74ceae628
rsuntk <rissu.ntk@gmail.com>
2026-08-27 18:01:40 +0000

    Revert "sched/core: Allow try_invoke_on_locked_down_task() with irqs disabled"
    This reverts commit 40ed8605328f4f6f13facb42e26e1290ef5414c8.
    
    Reason for revert: "Already applied"
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3139,7 +3139,7 @@ bool try_invoke_on_locked_down_task(struct task_struct *p, bool (*func)(struct t
 
 /**
  * try_invoke_on_locked_down_task - Invoke a function on task in fixed state
- * @p: Process for which the function is to be invoked, can be @current.
+ * @p: Process for which the function is to be invoked.
  * @func: Function to invoke.
  * @arg: Argument to function.
  *
@@ -3157,11 +3157,12 @@ bool try_invoke_on_locked_down_task(struct task_struct *p, bool (*func)(struct t
  */
 bool try_invoke_on_locked_down_task(struct task_struct *p, bool (*func)(struct task_struct *t, void *arg), void *arg)
 {
-	struct rq_flags rf;
 	bool ret = false;
+	struct rq_flags rf;
 	struct rq *rq;
 
-	raw_spin_lock_irqsave(&p->pi_lock, rf.flags);
+	lockdep_assert_irqs_enabled();
+	raw_spin_lock_irq(&p->pi_lock);
 	if (p->on_rq) {
 		rq = __task_rq_lock(p, &rf);
 		if (task_rq(p) == rq)
@@ -3178,7 +3179,7 @@ bool try_invoke_on_locked_down_task(struct task_struct *p, bool (*func)(struct t
 				ret = func(p, arg);
 		}
 	}
-	raw_spin_unlock_irqrestore(&p->pi_lock, rf.flags);
+	raw_spin_unlock_irq(&p->pi_lock);
 	return ret;
 }
 
