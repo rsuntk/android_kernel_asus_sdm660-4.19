8d92e61e3b10c3422e445edf1565a2d3e3568d10
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-09-09 02:04:01 +0600

    fuse: Use freezable wait for forced requests in request_wait_answer()
    Commit f5f8189d8c6f ("fuse: Use freezable wait in request_wait_answer()") made
    killable waits freezable when waiting for userspace FUSE responses. However,
    forced requests like fuse_flush() set FR_FORCE and bypass that branch and calls
    wait_event().
    
    Since wait_event() uses an uninterruptible sleep without freezer bits, a task
    closing a FUSE file while the FUSE daemon is being frozen cannot be frozen by the
    kernel, which leads to the following call traces:
    
    [64650.467224] Freezing of tasks failed after 1.006 seconds (1 tasks refusing to freeze, wq_busy=0):
    [64650.467860] task:Thread-4        state:D stack:0     pid:5552  ppid:1032   flags:0x04000801
    [64650.467870] Call trace:
    [64650.467893] __switch_to+0x114/0x130
    [64650.467907] __schedule+0x570/0x7c0
    [64650.467912] schedule+0x68/0xa0
    [64650.467928] fuse_simple_request+0x614/0x770
    [64650.467933] fuse_flush+0x240/0x6f0
    [64650.467941] filp_close+0x38/0xa0
    [64650.467950] __close_fd+0x34/0x40
    [64650.467953] __arm64_sys_close+0x1c/0x50
    [64650.467958] el0_svc_common+0xb0/0x120
    [64650.467961] do_el0_svc+0x18/0x20
    [64650.467965] el0_sync_handler+0x148/0x190
    [64650.467970] el0_sync+0x140/0x180
    
    Make this wait use freezable scheduling so the freezer can safely skip the task
    during suspend.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: rssu. <rissu.ntk@gmail.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -418,7 +418,7 @@ static void request_wait_answer(struct fuse_req *req)
 	 * Either request is already in userspace, or it was forced.
 	 * Wait it out.
 	 */
-	wait_event(req->waitq, test_bit(FR_FINISHED, &req->flags));
+	fuse_wait_event(req->waitq, test_bit(FR_FINISHED, &req->flags));
 }
 
 static void __fuse_request_send(struct fuse_req *req)
