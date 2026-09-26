8ff2cd2d27cf2db446980a84ff43cc4454b8ee01
rssu. <rissu.ntk@gmail.com>
2026-09-26 10:19:35 +0700

    Revert "block: refactor default iosched selection"
    This reverts commit ffc20a01f48bade2d2db994295be0f527a0edfac.
    
    Signed-off-by: rssu. <rissu.ntk@gmail.com>

--- block/elevator.c
+++ block/elevator.c
@@ -922,7 +922,14 @@ int elevator_init_mq(struct request_queue *q)
 	if (unlikely(q->elevator))
 		goto out;
 
-	e = elevator_get(q, CONFIG_BLOCK_DEFAULT_IOSCHED, false);
+#if defined(CONFIG_BFQ_DEFAULT)
+		e = elevator_get(q, "bfq", false);
+#elif defined(CONFIG_MQ_KYBER_DEFAULT)
+		e = elevator_get(q, "kyber", false);
+#else
+		e = elevator_get(q, "mq-deadline", false);
+#endif
+
 	if (!e)
 		goto out;
 	err = blk_mq_init_sched(q, e);
